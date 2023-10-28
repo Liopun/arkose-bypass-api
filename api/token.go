a986fe61573f1b8f8f11a77d4596c761ebb09941
Captone Habiyaremye <hcaptone@gmail.com>
2023-10-28 15:09:13 -0600

    Cors headers

--- api/token.go
+++ api/token.go
@@ -18,6 +18,17 @@ type SuccessResponse struct {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
+	// Set CORS headers to allow requests from specific origin (https://leetcode.com)
+	w.Header().Set("Access-Control-Allow-Origin", "https://leetcode.com")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+	// Handle CORS preflight requests
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	// Attempt to get the token
 	token, err := funcaptcha.GetOpenAITokenV2()
 	if err != nil {
